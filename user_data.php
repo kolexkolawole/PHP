3cfbff8258a80bb0f429cc13fda2b3eaf65abab5
kolexkolawole <kolexkolawole@gmail.com>
2022-10-15 04:52:29 +0100

    commit

--- user_data.php
+++ user_data.php
@@ -2,16 +2,26 @@
 <body>
 
 <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-  Name: <input type="text" name="fname">
+  Name: <input type="text" name="userdata">
   <input type="submit"> <br><br>
-  Name: <input type="text" name="fname">
-  <input type="submit">
+
+  Email: <input type="text" name="userdata.csv">
+  <input type="submit"> <br><br>
+
+  Date of Birth: <input type="text" name="userdata.csv">
+  <input type="submit"> <br><br>
+
+  Gender: <input type="text" name="userdata">
+  <input type="submit">  <br><br>
+
+  Country: <input type="text" name="userdata">
+  <input type="submit">  <br><br>
 </form>
 
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   // collect value of input field
-  $name = $_POST['fname'];
+  $name = $_POST['submit'];
   if (empty($name)) {
     echo "Name is empty";
   } else {
