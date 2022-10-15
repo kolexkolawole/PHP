335bb23e3e648a57e5df0cf22d05bffe298deea7
kolexkolawole <kolexkolawole@gmail.com>
2022-10-15 05:23:22 +0100

    commit

--- user_data.php
+++ user_data.php
@@ -1,31 +1,47 @@
 <html>
 <body>
-
+<center> <h1>My Registration Form</h1></center>
 <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-  Name: <input type="text" name="userdata">
+  Name: <input type="text" name="fname">
   <input type="submit"> <br><br>
 
-  Email: <input type="text" name="userdata.csv">
+  Email: <input type="text" Email="fname">
   <input type="submit"> <br><br>
 
-  Date of Birth: <input type="text" name="userdata.csv">
+  DOB: <input type="number" Email="fnumber">
   <input type="submit"> <br><br>
 
-  Gender: <input type="text" name="userdata">
+  Gender: <input type="text" Gender="fname">
   <input type="submit">  <br><br>
 
-  Country: <input type="text" name="userdata">
+  Country: <input type="text" Country="fname">
   <input type="submit">  <br><br>
 </form>
 
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    {
   // collect value of input field
-  $name = $_POST['submit'];
+  $name = $_POST['fname'];
   if (empty($name)) {
     echo "Name is empty";
   } else {
     echo $name;
+  }
+}{
+  $email = $_POST['fname'];
+  if (empty($email)) {
+    echo "Email is empty";
+  } else {
+    echo $email;
+  }
+}{
+  $dob = $_POST['fnumber'];
+  if (empty($dob)) {
+    echo "Date of Birth is empty";
+  } else {
+    echo $dob;
+  }
   }
 }
 ?>
