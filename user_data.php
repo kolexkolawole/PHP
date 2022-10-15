0bbf41003b2ce712c91336654e088bfad5f35bcb
kolexkolawole <kolexkolawole@gmail.com>
2022-10-15 04:29:09 +0100

    commit

--- user_data.php
+++ user_data.php
@@ -1,40 +1,24 @@
-
-!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content"IE=edge"> 
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>learning php</title>
-
-</head>
+<html>
 <body>
-<form action="form.php" method="GET">
+
+<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+  Name: <input type="text" name="fname">
+  <input type="submit"> <br><br>
+  Name: <input type="text" name="fname">
+  <input type="submit">
+</form>
 
 <?php
-
-//get the data from the submitted form
-
-$num1 = $_POST['num1'];
-
-$num2 = $_POST['num2'];
-
-$sum = $num1 + $num2;
-
-$difference = $num1 - $num2;
-
-$product = $num1 * $num2;
-
- 
-
-echo "SUM: " . $sum . "<br>";
-echo "DIFFERENCE: " . $difference . "<br>";
-echo "PRODUCT: " . $product . "<br>";
-
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // collect value of input field
+  $name = $_POST['fname'];
+  if (empty($name)) {
+    echo "Name is empty";
+  } else {
+    echo $name;
+  }
+}
 ?>
-
 
 </body>
 </html>
-
-
